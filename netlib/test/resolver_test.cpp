fac24b92cf371d920ecc4fe7960b72c9d657780c
yksz <yksz.dev@gmail.com>
2016-04-24 19:15:52 +0900

    test: resolver: Add the test code

---
+++ netlib/test/resolver_test.cpp
@@ -0,0 +1,20 @@
+#include "net/resolver.h"
+#include <string>
+#include <gtest/gtest.h>
+
+using namespace net;
+
+TEST(LookupAddress, Localhost) {
+    {
+        std::string addr;
+        error err = LookupAddress("localhost", &addr);
+        EXPECT_EQ(error::nil, err);
+        EXPECT_EQ("127.0.0.1", addr);
+    }
+    {
+        std::string addr;
+        error err = LookupAddress("127.0.0.1", &addr);
+        EXPECT_EQ(error::nil, err);
+        EXPECT_EQ("127.0.0.1", addr);
+    }
+}
