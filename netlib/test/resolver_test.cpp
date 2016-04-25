958976186e8a3702852805e446039b1dddaeff10
yksz <yksz.dev@gmail.com>
2016-04-25 22:55:36 +0900

    refactor/test: Refactor the resolver test code

--- netlib/test/resolver_test.cpp
+++ netlib/test/resolver_test.cpp
@@ -5,18 +5,19 @@
 using namespace net;
 
 TEST(LookupAddress, Localhost) {
-    {
-        std::string addr;
-        error err = LookupAddress("localhost", &addr);
-        EXPECT_EQ(error::nil, err);
-        EXPECT_EQ("127.0.0.1", addr);
-    }
-    {
-        std::string addr;
-        error err = LookupAddress("127.0.0.1", &addr);
-        EXPECT_EQ(error::nil, err);
-        EXPECT_EQ("127.0.0.1", addr);
-    }
+    std::string addr;
+    error err = LookupAddress("localhost", &addr);
+
+    EXPECT_EQ(error::nil, err);
+    EXPECT_EQ("127.0.0.1", addr);
+}
+
+TEST(LookupAddress, LocalIPv4) {
+    std::string addr;
+    error err = LookupAddress("127.0.0.1", &addr);
+
+    EXPECT_EQ(error::nil, err);
+    EXPECT_EQ("127.0.0.1", addr);
 }
 
 TEST(LookupAddress, Nullptr) {
