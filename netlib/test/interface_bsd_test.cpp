f561d13e6bb39d9d3b2921fbe3bc4d68c260e00c
yksz <yksz.dev@gmail.com>
2016-04-25 22:58:22 +0900

    test/interface: Add the test code for Linux and BSD

---
+++ netlib/test/interface_bsd_test.cpp
@@ -0,0 +1,20 @@
+#include "net/interface.h"
+#include <array>
+#include <string>
+#include <gtest/gtest.h>
+
+using namespace net;
+
+TEST(NetworkInterface, GetByName) {
+    const std::string name = "lo0";
+
+    NetworkInterface inf;
+    error err = GetNetworkInterfaceByName(name, &inf);
+
+    EXPECT_EQ(error::nil, err);
+    EXPECT_EQ(name, inf.name);
+    std::array<unsigned char, 6> macAddr{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+    EXPECT_EQ(macAddr, inf.hardwareAddress);
+    EXPECT_EQ(true, inf.isUp);
+    EXPECT_EQ(true, inf.isLoopback);
+}
